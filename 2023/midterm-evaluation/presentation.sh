41df56d555bb116990e0e64e721e09beefb03fc4
engeir <engeir@pm.me>
2023-03-15 10:55:14 +0100

    chore: example of sent presentation

--- 2023/midterm-evaluation/presentation.sh
+++ 2023/midterm-evaluation/presentation.sh
@@ -19,17 +19,67 @@
 echo "Global Temperature Response to Volcanic Activity" >sent.sent
 echo "" >>sent.sent
 
-pnglatex -S -b Transparent -F White -d 2000 -f "\mathrm{CO_2}" -o pic/co2.png
-printf "@pic/co2.png\n\n" >>sent.sent
+# Create all image files
+pnglatex -S -e displaymath -b Transparent -F White -d 2000 -f "\Delta T(t)=\int_{\infty}^{t}G(t-s)[F(s)\mathrm{d}s+\sigma \mathrm{d}B(s)]" -o pic/gen/lin_model.png
+pnglatex -S -e displaymath -b Transparent -F White -d 2000 -f "T_K(t)=\sum_{k=1}^K A_k \phi\left( \frac{t-t_k}{\tau_\mathrm{d}} \right)" -o pic/gen/sum.png
+pnglatex -S -e displaymath -b Transparent -F White -d 2000 -f "T_K(t)=[\phi*f_K]\left(\frac{t}{\tau_\mathrm{d}}\right)" -o pic/gen/conv.png
+pnglatex -S -e displaymath -b Transparent -F White -d 2000 -f "\phi^{(n+1)}=\phi^{(n)}\frac{(T_K-\langle T_K\rangle)*\hat{f}_K+b}{\phi^{(n)}*f_K*\hat{f}_K+b}" -o pic/gen/deconv.png
 
-pnglatex -S -b Transparent -F White -d 2000 -f "T_K(t)=\sum_{k=1}^K A_k \phi\left( \frac{t-t_k}{\tau_\mathrm{d}} \right)" -o pic/sum.png
-printf "@pic/sum.png\n\n" >>sent.sent
+# Generate the text file.
+echo "Motivation and background
 
-pnglatex -S -b Transparent -F White -d 2000 -f "T_K(t)=[\phi*f_K]\left(\frac{t}{\tau_\mathrm{d}}\right)" -o pic/conv.png
-printf "@pic/conv.png\n\n" >>sent.sent
+@pic/gen/lin_model.png
 
-pnglatex -S -b Transparent -F White -d 2000 -f "\phi^{(n+1)}=\phi^{(n)}\frac{(T_K-\langle T_K\rangle)*\hat{f}_K+b}{\phi^{(n)}*f_K*\hat{f}_K+b}" -o pic/deconv.png
-printf "@pic/deconv.png\n\n" >>sent.sent
+Filtered Poisson Process
+
+@pic/gen/sum.png
+
+@pic/gen/conv.png
+
+Deconvolution algorithm
+
+@pic/gen/deconv.png
+
+Model setup
+  1. Synthetic eruptions are given to CESM2
+  2. Run using the BWma1850 compset
+
+Results
+
+Single-volcano events
+
+Double-volcano events
+
+Do non-linear effects become important as an eruption
+occur when the temperature is in a perturbed state?
+
+CESM1 Last Millennium Ensemble
+
+@./pic/cesm_lme_deconvolution_delta_old-daily-3000it_zoomed-frc.png
+
+@./pic/cesm_lme_deconvolution_delta_new2_zoomed-temp.png
+
+@./pic/cesm_lme_deconvolution_delta_new2_zoomed.png
+
+Deconvolution Issues
+
+@./pic/f_orig.png
+
+@./pic/f_forward.png
+
+Going Forward
+
+Paper 1
+Linear response to volcanic forcing
+in different climate states in CESM2
+
+Paper 2
+Pulse estimation from simulation data
+with a deconvolution method
+
+Paper 3
+Assessment of non-linear responses
+in the LongRunMIP ensemble" >>sent.sent
 
 # Finally, why not have a look at the result!
-# sent ./sent.sent
+sent ./sent.sent
